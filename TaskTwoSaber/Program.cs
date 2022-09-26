b4ae0b75d5bedee9a334a4b93b66145dad85ff55
PC <PC@LAPTOP-IGSE4RMJ>
2022-09-26 18:28:16 +0300

    Add project files.

---
+++ TaskTwoSaber/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace TaskTwo
+{
+    class Programm
+    {
+        static void Main()
+        {
+            var data = new StringBuilder("AAA BBB AAA");
+
+            RemoveDups(data);
+
+            Console.WriteLine(data); // "A B A"
+        }
+        static void RemoveDups(StringBuilder str)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(str[0]);
+            for(int index = 1; index < str.Length; index++) 
+            {
+                if(str[index] != sb[sb.Length-1]) 
+                {
+                    sb.Append(str[index]);
+                }
+            }
+
+            str.Replace(str.ToString(),sb.ToString());
+        }
+    }
+}
